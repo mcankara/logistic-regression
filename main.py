a27b9ec64631c29effa88c6ba4fb7e9d69b33927
N. Mertcan Cankara <mcankara93@gmail.com>
2018-11-16 03:47:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from lr_utils import load_dataset
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
 # Example of a picture
-index = 25
-plt.imshow(train_set_x_orig[index])
+# index = 25
+# plt.imshow(train_set_x_orig[index])
 # print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode(
 #     "utf-8") + "' picture.")
 
